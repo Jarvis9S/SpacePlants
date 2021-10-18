dd2c58c9767179535a3bd4d8ae65b63cc4349af5
Jarvis <martinsebastianj@gmail.com>
2021-10-18 21:54:13 +0200

    0.1.1.6
    Actualizar PG a V2

--- backend/databasePool.js
+++ backend/databasePool.js
@@ -3,4 +3,11 @@ const databaseConfiguration = require("./hidden/databaseConfiguration.js");
 
 const pool = new Pool(databaseConfiguration);
 
+pool.connect();
+
 module.exports = pool;
+
+pool.query("SELECT * FROM traveltable", (error, response) => {
+  if (error) return console.log(error);
+  console.log(response.rows);
+});
