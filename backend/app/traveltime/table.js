1a77d2210f491100ad30fa39bc8e57e1b4776911
Jarvis <martinsebastianj@gmail.com>
2021-10-18 22:46:09 +0200

    0.1.2
    Fixed connection

--- backend/app/traveltime/table.js
+++ backend/app/traveltime/table.js
@@ -3,7 +3,6 @@ const pool = require("../../databasePool.js");
 class TravelTable {
   static storeTravel(travel) {
     pool.query(
-      console.log("hi"),
       "INSERT INTO traveltable(expiration) VALUES($1)",
       [travel.expiration],
       (error, response) => {
