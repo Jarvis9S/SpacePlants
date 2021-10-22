22955180a60e11917a2b8bf69b22ba9df86dbd32
Jarvis <martinsebastianj@gmail.com>
2021-10-22 22:07:38 +0200

    0.3.5b
    Bootstrap 5

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import {render} from "react-dom";
 import Travel from "./components/Travel.js";
 import Plant from "./components/Plant.js";
-
+import "./index.css";
 
 render(
     <div>
-        <h2>Plants from React!</h2>
+        <h2>Plants in space!</h2>
         <Travel />
         <Plant />
     </div>,
