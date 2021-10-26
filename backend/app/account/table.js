b340c95bc99e1919688471417c534f307e703240
Jarvis <martinsebastianj@gmail.com>
2021-10-26 22:30:19 +0200

    Update table.js

--- backend/app/account/table.js
+++ backend/app/account/table.js
@@ -4,7 +4,7 @@ class AccountTable{
     static storeAccount({ username, passwordis }) {
         return new Promise((resolve, reject) => {
             pool.query(
-                `INSERT INTO account(username, passwordis) VALUES VALUES($1,$2)`,
+                "INSERT INTO account(username, passwordis) VALUES ($1, $2)",
                 [username, passwordis],
                 (error, response) => {
                     if (error) return reject(error);
