31b85214c2dd3020ff971554de823e755cb8f7b9
Jarvis <martinsebastianj@gmail.com>
2021-10-26 22:30:10 +0200

    Update configure_db.sh

--- backend/bin/configure_db.sh
+++ backend/bin/configure_db.sh
@@ -7,7 +7,7 @@ createdb spaceplantsdb
 
 echo "Configuring spaceplantsdb..."
 
-psql -U node_user spaceplantsdb < .bin/sql/account.sql
+psql -U node_user spaceplantsdb < ./bin/sql/account.sql
 psql -U node_user spaceplantsdb < ./bin/sql/travel.sql
 psql -U node_user spaceplantsdb < ./bin/sql/plant.sql
 psql -U node_user spaceplantsdb < ./bin/sql/trait.sql
